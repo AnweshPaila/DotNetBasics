af270e5b5ae0f41941d250be2367d6498c276b67
Anwesh Paila <apaila@wsandco.com>
2021-05-19 15:00:53 +0530

    constructors, method, arrayswithuserinput, modifiers

---
+++ Inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             * Inheritance 
+             * It is a kind of relationship between one or more classes, which allows to inherit code from one class to other
+             */
+
+            /* One of the advantage of inheritance is code re usability */
+
+
+        }
+    }
+}
